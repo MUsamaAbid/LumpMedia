6131931c82262cd66353f111e7bc0b8a3f163775
MUsamaAbid <m.usamaabid.ua@gmail.com>
2023-11-03 11:22:24 +0500

    Before question response submssion

--- LumpGuessMultiplayer/Assets/My project/Scripts/Manager.cs
+++ LumpGuessMultiplayer/Assets/My project/Scripts/Manager.cs
@@ -11,7 +11,12 @@ public class Manager : MonoBehaviourPun
 
     [SerializeField] PhotonView pv;
 
+    [SerializeField] GameObject QuestionBox;
+
     [SerializeField] Text Question;
+
+    [SerializeField] InputField Answer;
+    [SerializeField] Button SubmitAnswerButton;
 
     private void Start()
     {
@@ -27,18 +32,38 @@ public class Manager : MonoBehaviourPun
     }
     void SpawnPlayer()
     {
-        int r = Random.Range(-10, 10);
-        Vector3 pos = new Vector3(PlayerPrefab.transform.position.x + r, PlayerPrefab.transform.position.y, PlayerPrefab.transform.position.z);
+        int r = Random.Range(-8, 8);
+        Vector3 pos = new Vector3(PlayerPrefab.transform.position.x - 3 + r, PlayerPrefab.transform.position.y, PlayerPrefab.transform.position.z);
         PhotonNetwork.Instantiate(PlayerPrefab.name, pos, PlayerPrefab.transform.rotation);
     }
     public void OnClickDisplayQuestion()
     {
+        ShowQuestionButton.SetActive(false);
         Question.text = "This is the question";
+        QuestionBox.SetActive(true);
         pv.RPC("DisplayQuestion", RpcTarget.Others); //Not all because we are already flipping it locally
+        pv.RPC("EnableQuestionBox", RpcTarget.Others); //Not all because we are already flipping it locally
     }
     [PunRPC]
     void DisplayQuestion()
     {
         Question.text = "This is the question";
+    }
+    private void Update()
+    {
+        if(Answer.text.Length > 0)
+        {
+            SubmitAnswerButton.interactable = true;
+        }
+        else
+        {
+            SubmitAnswerButton.interactable = false;
+        }
+    }
+
+    [PunRPC]
+    void EnableQuestionBox()
+    {
+        QuestionBox.SetActive(true);
     }
 }
